fcab0fb512e9b7370737f2a71704c259b6f9b378
Manthan Sharma <152221156+manthansharma17@users.noreply.github.com>
2026-01-25 22:03:25 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showFront() {
+    Document.getElementById("main-1").src = "/images/Grey H F.png";
+}
+function showBack() {
+    Document.getElementById("main-1").src = "/images/Grey H B.png";
+}
